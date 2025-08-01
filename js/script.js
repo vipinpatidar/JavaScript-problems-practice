34b7be0dd02b2c7d9c50e82e93ed9a0745b86209
Vipin Patidar <99580663+vipinpatidar@users.noreply.github.com>
2025-08-01 16:18:28 +0000

    created initial ui ( dark mode) styling

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("Hey JavaScript");
+const darkLightBtn = document.querySelector(".dark-light__btn");
+
+darkLightBtn.addEventListener("click", function(){
+    document.documentElement.classList.toggle("light-theme");
+    darkLightBtn.textContent = darkLightBtn.textContent !== "☀️" ? "☀️" : "🌙"
+})
+
+
+
+/*
+Write a JavaScript program to copy a string to the clipboard.
+*/
+const copyBtn = document.querySelector(".copy__btn");
+const copyPera = document.querySelector(".copy__pera");
+
+copyBtn.addEventListener("click", function(){
+    const text = copyPera.textContent;
+    navigator.clipboard.writeText(text);
+})
+
+
